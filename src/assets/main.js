4ee29e5753f78336614a6d17c712d22cf4c70506
Miguel Angel Osonio <miguelos1128@gmail.com>
2022-12-16 21:51:52 -0600

    arague la foto de perfil de Omar

--- src/assets/main.js
+++ src/assets/main.js
@@ -22,16 +22,18 @@ async function fetchData(urlApi){
         let view = `
         ${videos.items.map(video => `   
             <div class="group relative">
-            <div
-                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-            </div>
+            <a href = "https://www.youtube.com/results?search_query=${video.snippet.title}">
+                <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                </div>
+            
             <div class="mt-4 flex justify-between">
                 <h3 class="text-sm text-gray-700">
                 <span aria-hidden="true" class="absolute inset-0"></span>
                 ${video.snippet.title}
                 </h3>
             </div>
+            </a>
             </div>
 
         `).slice(0, 8).join('')}
